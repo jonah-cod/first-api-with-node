005d13cdbbdf0e080453eff693b91b32327f6b6a
Jonathan Mwaniki <idolearndaily@gmail.com>
2023-02-28 12:40:02 +0300

    connecting to db in node and implementing endpoints with the databse

--- server.js
+++ server.js
@@ -1,71 +1,82 @@
 const express = require('express');
+require('dotenv').config()
+const sql = require('mssql');
+const { config } = require('./sqlconfig')
+
 
 const app = express();
-
 app.use(express.json())
-const users = [
-      {
-            name: "mahesh",
-            password: "password1",
-            profession: "teacher",
-            id: 1
-      },
-      {
-            name: "suresh",
-            password: "password2",
-            profession: "librarian",
-            id: 2
-      },
-      {
-            name: "ramesh",
-            password: "password3",
-            profession: "clerk",
-            id: 3
-      }
-]
+
 // CRUD = CREATE(POST), READ(GET), UPDATE(PUT/PATCH), DELETE(DELETE)
 app.get('/', (req, res)=>{
       //handling requests
       res.send("Hello, Welcome!")
 })
 
-app.get('/users', (req, res)=>{
-      res.json(users)
+app.get('/users', async(req, res)=>{
+      try {
+            await sql.connect(config)
+            let users = await sql.query`SELECT * FROM users`;
+            
+            res.json(users.recordset)
+      } catch (error) {
+            console.log(error)
+      }   
 })
 
-app.post('/users', (req, res)=>{
+app.post('/users', async(req, res)=>{
       //reading the request body
       const details = req.body;
-      users.push(details);
-      res.json(users)
+
+      try{
+      await sql.connect(config);
+      let results = await sql.query`INSERT INTO users VALUES(${details.name}, ${details.profession}, ${details.password})`
+
+      if(results.rowsAffected.length) res.json({success: true, message: 'user created successfully'})
+      
+      } catch(error){
+            console.log(error)
+      }
 })
 
-app.get('/users/:id', (req, res)=>{
+app.get('/users/:id', async (req, res)=>{
       const {id} = req.params
-      const user = users.find(user=> user.id === Number(id))
-      res.json(user);
+      
+      try {
+            await sql.connect(config);
+            let results = await sql.query`SELECT * FROM users WHERE id = ${id}`
+            res.json(results.recordset[0])
+      } catch (error) {
+            
+      }
 })
 
-app.put('/users', (req, res)=>{
+app.put('/users', async(req, res)=>{
       const user_details = req.body
-      users.map(user=>{
-            if (user.id === user_details.id) {
-                  user.name=user_details.name
-                  user.profession=user_details.profession
-                  return user
-            } else {
-                  return user
-            }
-      })
-      res.json(users);
+      try {
+            await sql.connect(config);
+            let results = await sql.query`UPDATE users  
+                                                SET full_names =${user_details.name},
+                                                profession = ${user_details.profession}
+            WHERE id=${user_details.id}`
+            if(results.rowsAffected.length) res.json({success: true, message: 'user updated successfully'})
+      } catch (error) {
+            console.log(error)
+      }
 })
 
-app.delete('/users/:id', (req, res)=>{
-      const {id} = req.params
-      let new_users = users.filter(user=> user.id !== Number(id))
-      res.json(new_users);
+app.delete('/users/:id',async(req, res)=>{
+      const {id} = req.params;
+      
+      try {
+            await sql.connect(config)
+            let results = await sql.query`DELETE FROM users WHERE id = ${id}`
+            if(results.rowsAffected.length) res.json({success: true, message: 'user deleted successfully'})
+      } catch (error) {
+            console.log(error)
+      }
 })
 
 //starting the server and listening to requests
-const port = 4040;
+const port = process.env.PORT || 4080;
 app.listen(port, ()=>{console.log(`Server listening on port: ${port}`)})
